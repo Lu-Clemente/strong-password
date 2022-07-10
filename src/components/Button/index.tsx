b82b1b395af5585140d67fc35908c7145524c8d6
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-07-09 22:25:21 -0300

    Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Label } from "./styles";
+
+const Button = () => {
+    return (
+        <Container>
+            <Label>Confirmar</Label>
+        </Container>
+    )
+}
+
+export default Button;
